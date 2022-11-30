d9188d81152b193696469190b7a4ee8b0d7ddeb5
Daniya <daniyashagieva@gmail.com>
2022-11-30 21:51:48 +0300

    added answer/qustion styles and usestate

--- study-stack/src/components/WordCard/WordCard.jsx
+++ study-stack/src/components/WordCard/WordCard.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 
 export default function WordCard({english, transcription, russian}) {
 
+  //для перевода
   const [translation, setRussian] = useState(false)
   const handleChange = () => {
     setRussian(!translation)
@@ -16,21 +17,44 @@ export default function WordCard({english, transcription, russian}) {
     setChecked(!checked)
   }
 
+  //для кнопки
   card__translation = checked ? "card__translation" : "card__button"
 
+  //общая функция обработчик на кнопку
   const handleTranslation = () => {
     handleChecked()
     handleChange()
+  }
+
+  //для переворачивания карточки с переводом
+  const [answer, setAnswer] = useState(false)
+  const handleAnswer = () => {
+    setAnswer(!answer)
+  }
+
+  //для стилей question/answer
+  let card__text = "question__text"
+  const [answered, setAnswered] = useState(false)
+
+  const handleAnswered = () => {
+    setAnswered(!answered)
+  }
+
+  card__text = answered ? "answer__text" : "question__text"
+
+  const handleAnsWords = () => {
+    handleAnswer()
+    handleAnswered()
   }
 
   return (
     <div className={style.container}>
       <div className={style.wrapper}>
       <div className={style.card}>
-      <span className={style.card__word}>{english}</span>
-      <span className={style.card__transcription}>{transcription}</span>
+      <span onClick={handleAnsWords} className={card__text}>{answer ? "Answer" : "Question"}</span>
+      <span className={style.card__word}>{answer ? russian : english}</span>
+      <span className={style.card__transcription}>{answer ? "" : transcription}</span>
       <div className={style.btn__container}>
-      {/* <button className={[style.card__button, card__translation].join(' ')} onClick={handleTranslation}>{translation ? russian : "Click to flip"}</button> */}
       <button className={card__translation} onClick={handleTranslation}>{translation ? russian : "Click to flip"}</button>
       </div>
       </div>
