021a4c8280736dbb597fd3cbdf07088b78bd7be7
Daniya <daniyashagieva@gmail.com>
2022-12-20 21:35:38 +0300

    navlink

--- study-stack/src/components/Menu/Menu.jsx
+++ study-stack/src/components/Menu/Menu.jsx
@@ -6,11 +6,11 @@ export default function Menu() {
     <div>
         <header className={style.header}>
     <div className={style.header__wrapper}>
-    <Link to="/"><img src='./assets/word_logo.webp' alt="logo" className={style.header__logo}/></Link>
+    <Link to="."><img src='./assets/word_logo.webp' alt="logo" className={style.header__logo}/></Link>
     <ul className={style.header__nav}>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/words">Words</Link></li>
-      <li><Link to="/flashcards">Flashcards</Link></li>
+      <li><Link to=".">Home</Link></li>
+      <li><Link to="words">Words</Link></li>
+      <li><Link to="flashcards">Flashcards</Link></li>
     </ul>
     </div>
   </header>
