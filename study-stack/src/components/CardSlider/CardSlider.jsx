9380d2417b942466443b70f1a1f711b3029f7e8a
Daniya <daniyashagieva@gmail.com>
2023-01-22 18:48:22 +0300

    words counter

--- study-stack/src/components/CardSlider/CardSlider.jsx
+++ study-stack/src/components/CardSlider/CardSlider.jsx
@@ -14,10 +14,14 @@ export default function CardSlider({
   const [cardIndex, setCardIndex] = useState(0);
   const [checked, setChecked] = useState(false);
   const [answered, setAnswered] = useState(false);
+  const [learntWords, setLearntWords] = useState(false)
+  const [learntWordsIndex, setLearntWordsIndex] = useState([])
+  const [wordsQuantity, setWordsQuantity] = useState(0);
 
   const handleChange = () => {
     setRussian(!translation);
     setAnswer(false);
+    handleCount(words[cardIndex].id)
     handleCheckedStyle();
   };
 
@@ -55,19 +59,45 @@ export default function CardSlider({
     setAnswered(!answered);
   };
 
+//посчитать выученные слова
+const handleCount = (id) => {
+  const wordsArr = [...learntWordsIndex]
+  wordsArr.push(id)
+  let resultArr = []
+
+  wordsArr.forEach((el) => {
+    if (!resultArr.includes(el)) {
+      resultArr.push(el)
+    }
+  })
+
+  setLearntWordsIndex(resultArr)
+  setWordsQuantity(resultArr.length)
+
+  //проверить, все ли слова выучили
+  if (resultArr.length === words.length) {
+    setLearntWords(true)
+  }
+}
+
   return (
     <div className={style.container}>
+      <div className={style.wrapper}>
       <button onClick={handleClickPrevious} className={style.button}>
         <div className={style.arrow__prev}></div>
       </button>
       <WordCard
         english={words[cardIndex].english}
         transcription={words[cardIndex].transcription}
-        russian={words[cardIndex].russian} handleChange={handleChange} translation={translation} handleAnswer={handleAnswer} answer={answer} checked={checked} answered={answered}
+        russian={words[cardIndex].russian} handleChange={handleChange} translation={translation} handleAnswer={handleAnswer} answer={answer} checked={checked} answered={answered} 
       />
       <button onClick={handleClickNext} className={style.button}>
         <div className={style.arrow__next}></div>
       </button>
+      </div>
+      {learntWords
+      ? <span className={style.words_counter}>You've learnt all the words!</span>
+      : <span className={style.words_counter}>You've learnt: {wordsQuantity} / {words.length}</span>}
     </div>
   );
 }
