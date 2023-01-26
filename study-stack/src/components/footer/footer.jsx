c6cdddc64393e3a8b5abb4d4d22fd437e33dfb16
Daniya <daniyashagieva@gmail.com>
2023-01-26 14:42:32 +0300

    added copyrigth

--- study-stack/src/components/footer/footer.jsx
+++ study-stack/src/components/footer/footer.jsx
@@ -2,11 +2,14 @@ import style from './footer.module.scss'
 import { Link } from 'react-router-dom'
 
 export default function Footer() {
+
+  const today = new Date();
+
   return (
     <div  className={style.footer}>
       <div className={style.footer__wrapper}>
       <Link to="/"><img src='./assets/word_logo.webp' alt="logo" className={style.footer__logo}/></Link>
-      <span className={style.footer__text}>Created by Daniya Shagieva ⓒ 2022</span>
+      <span className={style.footer__text}>Created by Daniya Shagieva &copy; {today.getFullYear()}</span>
       </div>
     </div>
   )
